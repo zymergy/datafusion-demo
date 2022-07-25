b759c5ad402d04424dae873be1884962bbcde3f3
zymonster <>
2022-07-25 02:05:14 +0000

    setup working CSV example

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,9 @@ async fn main() -> datafusion::error::Result<()> {
   let ctx = SessionContext::new();
   ctx.register_csv("example", "tests/example.csv", CsvReadOptions::new()).await?;
 
-  // create a plan to run a SQL query
-  let df = ctx.sql("SELECT a, MIN(b) FROM example GROUP BY a LIMIT 100").await?;
+  // create a plan to run a SQL q&uery
+  let query = String::from("SELECT \"Column A\", MIN(\"Column B\") FROM example GROUP BY \"Column A\" LIMIT 100");
+  let df = ctx.sql(&query).await?;
 
   // execute and print results
   df.show().await?;
